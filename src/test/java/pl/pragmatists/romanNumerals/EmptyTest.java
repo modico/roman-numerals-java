f07883d20a8a356bd14e0d0576d961c4791936cc
michal.lipski <kiwaczki@gmail.com>
2021-04-22 22:23:40 +0200

    initial

---
+++ src/test/java/pl/pragmatists/romanNumerals/EmptyTest.java
@@ -0,0 +1,16 @@
+package pl.pragmatists.romanNumerals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class EmptyTest {
+
+
+    @Test
+    public void empty_test() {
+        assertThat(true).isTrue();
+    }
+
+
+}
